32209238a30f422db66d038fb317814f1b2d4f12
LesserKnown0ne <ryan.harris1@my.uwi.edu>
2022-04-17 16:06:53 +0000

    Login and Signup Works

--- App/models/user.py
+++ App/models/user.py
@@ -1,19 +1,27 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String, nullable=False)
+    firstName = db.Column(db.String, nullable=False)
+    lastName = db.Column(db.String, nullable=False)
+    username =  db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
 
-    def __init__(self, username, password):
+    def __init__(self, firstName, lastName, username, password):
+        self.firstName = firstName
+        self.lastName = lastName
         self.username = username
         self.set_password(password)
+
 
     def toDict(self):
         return{
             'id': self.id,
-            'username': self.username
+            'username': self.username,
+            'firstName': self.firstName,
+            'lastName': self.lastName
         }
 
     def set_password(self, password):
--- App/views/api.py
+++ App/views/api.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, redirect, render_template, request, send_from_directory
+from flask import Blueprint, redirect, render_template, request, send_from_directory, url_for
+from App.controllers import user, auth
+from flask_login import login_required, current_user
 
 api_views = Blueprint('api_views', __name__, template_folder='../templates')
 
@@ -6,10 +8,28 @@ api_views = Blueprint('api_views', __name__, template_folder='../templates')
 def get_api_docs():
     return render_template('index.html')
 
-@api_views.route('/login', methods=['GET'])
+@api_views.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == "POST":
+        user = auth.authenticate(request.form["email"], request.form["password"])
+        if user:
+            auth.login_user(user, False)
+            return redirect('https://8080-amiraha-connectin-snq80gsoo8u.ws-us40.gitpod.io/profile')
+        else:
+            return ("Login Failed")
+    else:
+        return render_template('login.html')
 
-@api_views.route('/signUp', methods=['GET'])
+@api_views.route('/signUp', methods=['GET', 'POST'])
 def signUp():
-    return render_template('signUp.html')
+    if request.method == "POST":
+        user.create_user(request.form["fname"], request.form["lname"], request.form["email"], request.form["password"] )
+        return ("Signed in")
+    else:
+        return render_template('signUp.html')
+
+@api_views.route('/profile', methods=['GET'])
+@login_required
+def profile():
+    return ("Welcome " + current_user.firstName)
+
